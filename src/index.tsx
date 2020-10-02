16434bc0b573fd2779f6194cb21ff61579f86d28
Amber Kampen <akampen@g.hmc.edu>
2020-10-01 20:05:11 -0700

    added basic nav bar, not good w/ pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  NavigationBar from './components/NavigationBar';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavigationBar/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
