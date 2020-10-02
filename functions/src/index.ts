814c18bc882d5f768eb5af61e3771f5d2427bf42
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2020-10-02 10:17:49 -0500

    Switch HTTP request package from Gaxios to Axios

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import { request, GaxiosResponse } from  'gaxios';
+import axios from 'axios';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,22 +9,22 @@ import { request, GaxiosResponse } from  'gaxios';
 //   response.send("Hello from Firebase!");
 // });
 
-const THINGSPEAK_URL_TEMPLATE = "https://api.thingspeak.com/channels/<channel_id>/feeds.json";
-const CHANNEL_FIELD = "<channel_id>";
+const THINGSPEAK_URL_TEMPLATE: string = "https://api.thingspeak.com/channels/<channel_id>/feeds.json";
+const CHANNEL_FIELD: string = "<channel_id>";
 
 exports.thingspeakToFirestore = functions.pubsub.schedule("every 2 minutes").onRun(async (context) => {
-    const sensor_39193_primary_id = "865902";
+    const sensor_39193_primary_id: string = "865902";
     // This isn't actually a secret because anyone can find this on PurpleAir
-    const sensor_39193_primary_key = "GKAN7GY29LRG7CLK";
+    const sensor_39193_primary_key: string = "GKAN7GY29LRG7CLK";
     //const sensor_39193_secondary_id = "865903";
    // const sensor_39193_secondary_key = "6TNLM14BVTDHBURR";
-    const primary_res = await request({
+    const primary_res = await axios({
         url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, sensor_39193_primary_id),
         params: {
             api_key: sensor_39193_primary_key,
             results: 1
         }
-    }) as GaxiosResponse<any>; //TODO: Make this better by using an actual class here
+    }) //TODO: Make this better by using an actual class here
     console.log(primary_res);
     console.log(primary_res.data.feeds);
 });
