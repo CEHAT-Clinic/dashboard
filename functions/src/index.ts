b94cf7ff84c199fb2caee1a6641b897725555009
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-10-05 15:51:13 -0700

    Fix linting in home directory and update directory used for builds and hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,33 +10,6 @@ const db = admin.firestore();
 const THINGSPEAK_URL_TEMPLATE = "https://api.thingspeak.com/channels/<channel_id>/feeds.json";
 const CHANNEL_FIELD = "<channel_id>";
 
-exports.thingspeakToFirestore = functions.pubsub.schedule("every 2 minutes").onRun(async (context) => {
-    const sensorList =  (await db.collection("/sensors").get()).docs;
-    for (const knownSensor of sensorList) {
-        knownSensor.data
-        const thingspeakInfo: PurpleAirResponse  = await getThingspeakKeysFromPurpleAir(knownSensor.data()["purpleAirId"]);
-    
-        const channelAPrimaryData = await axios({
-            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelAPrimaryId),
-            params: {
-                api_key: thingspeakInfo.channelAPrimaryKey,
-                results: 1
-            }
-        })
-        const channelBPrimaryData = await axios({
-            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelBPrimaryId),
-            params: {
-                api_key: thingspeakInfo.channelBPrimaryKey,
-                results: 1
-            }
-        })
-        
-        const reading = new SensorReading(channelAPrimaryData,channelBPrimaryData, thingspeakInfo);
-        console.log(reading);
-    }
-});
-
-
 async function getThingspeakKeysFromPurpleAir(purpleAirId: string): Promise<PurpleAirResponse> {
     const PURPLE_AIR_API_ADDRESS = "https://www.purpleair.com/json";
 
@@ -49,3 +22,29 @@ async function getThingspeakKeysFromPurpleAir(purpleAirId: string): Promise<Purp
 
     return new PurpleAirResponse(purpleAirApiResponse);    
 }
+
+exports.thingspeakToFirestore = functions.pubsub.schedule("every 2 minutes").onRun(async (context) => {
+    const sensorList =  (await db.collection("/sensors").get()).docs;
+    for (const knownSensor of sensorList) {
+        knownSensor.data
+        const thingspeakInfo: PurpleAirResponse  = await getThingspeakKeysFromPurpleAir(knownSensor.data()["purpleAirId"]);
+    
+        const channelAPrimaryData = await axios({
+            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelAPrimaryId),
+            params: {
+                apiKey: thingspeakInfo.channelAPrimaryKey,
+                results: 1
+            }
+        })
+        const channelBPrimaryData = await axios({
+            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelBPrimaryId),
+            params: {
+                apiKey: thingspeakInfo.channelBPrimaryKey,
+                results: 1
+            }
+        })
+        
+        const reading = new SensorReading(channelAPrimaryData,channelBPrimaryData, thingspeakInfo);
+        console.log(reading);
+    }
+});
