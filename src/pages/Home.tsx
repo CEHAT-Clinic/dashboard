de9f975dd922a795ccf9e8bef4d953fb6a12c20a
Amber Kampen <akampen@g.hmc.edu>
2020-10-08 12:16:46 -0700

    added disclaimer to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Home: React.FC = () => (
     <div>
         <h1> Home Page</h1>
-        <p> Here ia a map </p>
+        <h1> NOTE: This Website Is Under Construction. Check Back in Spring 2021</h1>
+        <p> Here is a map (in the future) </p>
         <p> and some other stuff</p>
     </div>
 );
