a7cdb5383816132cdca2c78779abc75dd95d0495
Amber Kampen <akampen@g.hmc.edu>
2020-10-26 11:03:58 -0700

    HERE mapping setup, our api key not working, zoom level 2 works, others not

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,21 +1,50 @@
-import React, { Component, useEffect } from 'react';
-import Plot from 'react-plotly.js';
-import { Helmet } from "react-helmet";
+// src/DisplayMapClass.js
+import * as React from 'react';
 
-// import ScriptTag from 'react-script-tag';
+export default class Map extends React.Component {
+  mapRef = React.createRef<HTMLDivElement>();
 
-class Map extends Component {
+  state = {
+    // The map instance to use during cleanup
+    map: null as any
+  };
+
+  componentDidMount() {
+
+    const H = (window as any).H;        // H is used to make API calls
+    const platform = new H.service.Platform({ 
+        apikey: "{HERE-API-KEY}"      // default HERE credentials
+        //apikey: process.env.REACT_APP_HERE_API_KEY  // our HERE credentials
+    });
+
+    const defaultLayers = platform.createDefaultLayers();
+
+    // Create an instance of the map
+    const map = new H.Map(
+      this.mapRef.current,
+      defaultLayers.vector.normal.map,
+      {
+        // This map is centered over Europe
+        center: { lat: 33.9575, lng: -118.2106 },
+        zoom: 2,
+        pixelRatio: window.devicePixelRatio || 1
+      }
+    );
+ 
+    this.setState({ map });
+  }
+
+  componentWillUnmount() {
+    // Cleanup after the map to avoid memory leaks when this component exits the page
+    if(this.state.map != null){
+        this.state.map.dispose();
+    }
+  }
+
   render() {
     return (
-      <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/N1AL2EMvVy0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        <Helmet>
-          <script src="./map_script.js"></script>
-        </Helmet>
-          <h1> TESTING</h1>
-      </div>
+      // Set a height on the map so it will display
+      <div ref={this.mapRef} style={{ height: "400px", width: "400px" }} />
     );
   }
 }
-
-export default Map
