9320fa54e6e78e7f573c28a3f1195983cad428ed
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-10-29 12:11:49 -0700

    Add skeleton code for generateReadingsCsv function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,16 @@ import SensorReading from './sensor-reading';
 admin.initializeApp();
 const db = admin.firestore();
 
-const THINGSPEAK_URL_TEMPLATE = "https://api.thingspeak.com/channels/<channel_id>/feeds.json";
+const THINGSPEAK_URL_TEMPLATE = 
+    "https://api.thingspeak.com/channels/<channel_id>/feeds.json";
 const CHANNEL_FIELD = "<channel_id>";
 
 const READINGS_SUBCOLLECTION_TEMPLATE = "/sensors/<doc_id>/readings";
 const DOC_ID_FIELD = "<doc_id>";
 
-async function getThingspeakKeysFromPurpleAir(purpleAirId: string): Promise<PurpleAirResponse> {
+async function getThingspeakKeysFromPurpleAir(
+    purpleAirId: string
+    ): Promise<PurpleAirResponse> {
     const PURPLE_AIR_API_ADDRESS = "https://www.purpleair.com/json";
 
     const purpleAirApiResponse = await axios({
@@ -26,33 +29,59 @@ async function getThingspeakKeysFromPurpleAir(purpleAirId: string): Promise<Purp
     return new PurpleAirResponse(purpleAirApiResponse);    
 }
 
-exports.thingspeakToFirestore = functions.pubsub.schedule("every 2 minutes").onRun(async () => {
-    const sensorList =  (await db.collection("/sensors").get()).docs;
-    for (const knownSensor of sensorList) {
-        const thingspeakInfo: PurpleAirResponse  = await getThingspeakKeysFromPurpleAir(knownSensor.data()["purpleAirId"]);
-        const channelAPrimaryData = await axios({
-            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelAPrimaryId),
-            params: {
-                api_key: thingspeakInfo.channelAPrimaryKey, //eslint-disable-line @typescript-eslint/camelcase
-                results: 1
-            }
-        })
-        const channelBPrimaryData = await axios({
-            url: THINGSPEAK_URL_TEMPLATE.replace(CHANNEL_FIELD, thingspeakInfo.channelBPrimaryId),
-            params: {
-                api_key: thingspeakInfo.channelBPrimaryKey, //eslint-disable-line @typescript-eslint/camelcase
-                results: 1
-            }
-        })
-        
-        const reading = new SensorReading(channelAPrimaryData, channelBPrimaryData, thingspeakInfo);
-        
-        const resolvedPath = READINGS_SUBCOLLECTION_TEMPLATE.replace(DOC_ID_FIELD, knownSensor.id);
-        // Firebase doesn't support objects crerated using new
-        if((await db.collection(resolvedPath).where("timestamp", "==", reading.timestamp).get()).empty) {
+exports.thingspeakToFirestore = functions.pubsub
+    .schedule("every 2 minutes")
+    .onRun(async () => {
+        const sensorList =  (await db.collection("/sensors").get()).docs;
+        for (const knownSensor of sensorList) {
+            const thingspeakInfo: PurpleAirResponse = 
+                await getThingspeakKeysFromPurpleAir(
+                    knownSensor.data()["purpleAirId"]
+                    );
+            const channelAPrimaryData = await axios({
+                url: THINGSPEAK_URL_TEMPLATE.replace(
+                    CHANNEL_FIELD, 
+                    thingspeakInfo.channelAPrimaryId
+                    ),
+                params: {
+                    api_key: thingspeakInfo.channelAPrimaryKey, //eslint-disable-line @typescript-eslint/camelcase
+                    results: 1
+                }
+            })
+            const channelBPrimaryData = await axios({
+                url: THINGSPEAK_URL_TEMPLATE.replace(
+                    CHANNEL_FIELD, 
+                    thingspeakInfo.channelBPrimaryId
+                    ),
+                params: {
+                    api_key: thingspeakInfo.channelBPrimaryKey, //eslint-disable-line @typescript-eslint/camelcase
+                    results: 1
+                }
+            })
+            
+            const reading = SensorReading.fromPurpleAir(
+                channelAPrimaryData, 
+                channelBPrimaryData, 
+                thingspeakInfo
+                );
+            
+            const resolvedPath = READINGS_SUBCOLLECTION_TEMPLATE.replace(
+                DOC_ID_FIELD, 
+                knownSensor.id
+                );
+            // Firebase doesn't support objects crerated using new
+            const readingsRef =  db.collection(resolvedPath);
+            if ((await readingsRef
+                .where("timestamp", "==", reading.timestamp)
+                .get())
+                .empty) {
 
-            const firestoreSafeReading = Object.assign({}, reading)
-            await db.collection(resolvedPath).add(firestoreSafeReading);
-        }
+                const firestoreSafeReading = Object.assign({}, reading)
+                await readingsRef.add(firestoreSafeReading);
+            }
     }
 });
+
+exports.generateReadingsCsv = functions.pubsub
+    .topic("generate-readings-csv")
+    .onPublish(async () => {});
