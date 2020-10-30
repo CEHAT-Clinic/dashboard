71853c4f1fe2f22ceb97aecd8aa1ed4228ef5884
Amber Kampen <akampen@g.hmc.edu>
2020-10-30 12:20:50 -0700

    removed iframe from health page

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -3,11 +3,6 @@ import React from "react";
 const Health: React.FC = () => (
     <div>
         <h1> Health Info Page</h1>
-        {/* <!-- Begin 270towin.com 2020 Presidential Election Countdown Widget --> */}
-        <iframe src="https://www.270towin.com/2020-countdown-clock/widget300x200.php" width="300" height="215" frameBorder="0">
-    Browser not supported. Visit <a href="https://www.270towin.com/">www.270towin.com</a>
-        </iframe>
-        {/* <!-- End 270towin.com 2020 Presidential Election Countdown widget --> */}
     </div>
 );
 
