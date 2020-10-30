c4f4409258089dd6a346bbb980f008314f66c813
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-10-30 14:16:11 -0700

    Initial commit

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,31 +1,27 @@
 import React, {lazy, Suspense} from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 // Import Pages
-const About = lazy(() => import("../pages/About"));         //lazy imports to save on load time
-const Home = lazy(() => import("../pages/Home"));           //lazy imports to save on load time
-const Health = lazy(() => import("../pages/Health"));       //lazy imports to save on load time
-const Admin = lazy(() => import("../pages/Admin"));         //lazy imports to save on load time
-const Involved = lazy(() => import("../pages/Involved"));   //lazy imports to save on load time
-
+const About = lazy(() => import('../pages/About')); //lazy imports to save on load time
+const Home = lazy(() => import('../pages/Home')); //lazy imports to save on load time
+const Health = lazy(() => import('../pages/Health')); //lazy imports to save on load time
+const Admin = lazy(() => import('../pages/Admin')); //lazy imports to save on load time
+const Involved = lazy(() => import('../pages/Involved')); //lazy imports to save on load time
 
 const Routes = () => {
-    return(
-        <Router>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Switch>
-                    <Route path="/about" component={About} />
-                    <Route path="/health" component = {Health}/> 
-                    <Route path="/involved" component = {Involved}/>
-                    <Route path="/admin" component={Admin} />
-                    <Route path={["/home","/"]} component = {Home}/>
-                </Switch>
-            </Suspense>
-        </Router>
-    )
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/health" component={Health} />
+          <Route path="/involved" component={Involved} />
+          <Route path="/admin" component={Admin} />
+          <Route path={['/home', '/']} component={Home} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
 
-}
-
-
-export default Routes
+export default Routes;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Home: React.FC = () => (
-    <div>
-        <h1> Home Page</h1>
-        <h2> NOTE: This Website Is Under Construction. Check Back in Spring 2021</h2>
-        <p> Here is a map (in the future) </p>
-        <p> and some other stuff</p>
-    </div>
+  <div>
+    <h1> Home Page</h1>
+    <h2>
+      {' '}
+      NOTE: This Website Is Under Construction. Check Back in Spring 2021
+    </h2>
+    <p> Here is a map (in the future) </p>
+    <p> and some other stuff</p>
+  </div>
 );
 
-export default Home
+export default Home;
