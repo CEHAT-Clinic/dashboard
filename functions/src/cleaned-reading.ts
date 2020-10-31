37ebba21829dfc3f76097ed8bf0a367c1a23b2d4
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2020-10-31 15:37:01 -0500

    Add new line at end of file

--- functions/src/cleaned-reading.ts
+++ functions/src/cleaned-reading.ts
@@ -1,11 +1,11 @@
 export default class CleanedReadings {
-    latitude: string;
-    longitude: string;
-    readings: number[];
-    
-    constructor(latitude: string, longitude: string, readings: number[]) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.readings = readings;
-    }
+  latitude: string;
+  longitude: string;
+  readings: number[];
+
+  constructor(latitude: string, longitude: string, readings: number[]) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.readings = readings;
+  }
 }
