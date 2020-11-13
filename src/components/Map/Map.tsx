867fcdbdaa73253d1b20603e029fce4cfcc89dfc
Amber Kampen <akampen@g.hmc.edu>
2020-11-13 12:01:55 -0800

    fixed most PR comments, still need to deal with API key and re-writing firebase function call

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import {db} from '../../firebase';
-
-// Map Style
-const mapStyle = {
-  height: '400px',
-};
+import {createIcon} from './marker_style';
 
 class Map extends React.Component {
-  mapRef = React.createRef<HTMLDivElement>(); // reference for div element
+  mapRef = React.createRef<HTMLDivElement>();
 
+  //state contains the instance of the HERE map to display
   state = {
-    map: null as H.Map,
+    map: null as H.Map | null,
   };
 
   // This fires every time the page is refreshed
   componentDidMount(): void {
-    console.log('Firing component did mount');
-
     const H = window.H; // H is used to make HERE API calls
 
-    // register our API key
+    // Register our API key
     const platform = new H.service.Platform({
-      apikey: process.env.REACT_APP_HERE_API_KEY,
+      apikey: String(process.env.REACT_APP_HERE_API_KEY),
     });
 
     const defaultLayers = platform.createDefaultLayers();
 
+    // Create a safe map reference that is guaranteed to not be null
+    const safeMapRef = this.mapRef.current ? this.mapRef.current : null;
+    if (safeMapRef === null) {
+      throw new Error('map reference is null');
+    }
+
     // Create an instance of the map
     const map = new H.Map(
-      this.mapRef.current, // Reference for Map
+      safeMapRef, // Reference for Map
       defaultLayers.vector.normal.map,
       {
         zoom: 13,
@@ -37,32 +38,13 @@ class Map extends React.Component {
       }
     );
 
-    // Creates the svg icon for a particular sensor given the label
-    // for the sensor (i.e. the current reading at that sensor)
-    function createIcon(label: string) {
-      //svg Marker Image (TODO: replace with better svg image)
-      const svgMarkup =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0' +
-        ' 0 24 24" fill="black" width="60px" height="60px"><path d="M0 0h24v' +
-        '24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7' +
-        ' 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1' +
-        '.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><text x="12"' +
-        ' y="18" font-size="4pt" font-family="Arial" font-weight="bold" ' +
-        'text-anchor="middle" fill="white">' +
-        label +
-        '</text></svg>';
-
-      const icon = new H.map.Icon(svgMarkup);
-      return icon;
-    }
-
     // Add the Sensor Markers to the map
     db.collection('current-reading')
       .limit(1) // Only one doc stored in current-readings
       .get()
       .then(querySnapshot =>
         querySnapshot.forEach(
-          // get doc from query
+          // Get doc from query
           doc => {
             if (doc.exists) {
               // Map of sensorID to readings and properties stored in data field
@@ -70,22 +52,21 @@ class Map extends React.Component {
 
               for (const sensorID in sensorMap) {
                 const sensorVal = sensorMap[sensorID];
-                // create label and icon for this marker
-                const label: string = String(sensorVal.readings[0]).split(
-                  '.'
-                )[0];
+                // The label for this sensor is the most recent hour average
+                // We strip to round to the ones place
+                const label = sensorVal.readings[0].toString().split('.')[0];
                 const icon = createIcon(label);
 
-                // add marker to map:
-                map.addObject(
-                  new H.map.Marker(
-                    {
-                      lat: sensorVal.latitude,
-                      lng: sensorVal.longitude,
-                    },
-                    {icon: icon}
-                  )
+                // Create marker
+                const marker = new H.map.Marker(
+                  {
+                    lat: sensorVal.latitude,
+                    lng: sensorVal.longitude,
+                  },
+                  {icon: icon}
                 );
+                // Add marker to the map
+                map.addObject(marker);
               }
             } else {
               // If doc.data() is undefined
@@ -105,6 +86,7 @@ class Map extends React.Component {
     // Resize map on screen resize
     window.addEventListener('resize', () => map.getViewPort().resize());
 
+    // Update state of React component to contain our map instead of null
     this.setState({map});
   }
 
@@ -118,7 +100,7 @@ class Map extends React.Component {
   render(): JSX.Element {
     return (
       <div>
-        <div ref={this.mapRef} style={mapStyle} />
+        <div ref={this.mapRef} style={{height: '400px'}} />
       </div>
     );
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,7 @@ import Map from '../components/Map/Map';
 const Home: React.FC = () => (
   <div>
     <h1> Home Page</h1>
-    <h2>
-      {' '}
-      NOTE: This Website Is Under Construction. Check Back in Spring 2021
-    </h2>
-    <p> Here is a map (in the future) </p>
-    <p> and some other stuff</p>
+    <h2>NOTE: This Website Is Under Construction. Check Back in Spring 2021</h2>
     <Map />
   </div>
 );
