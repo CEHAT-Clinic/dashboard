99bfd6b5718669d4b520d22c8c0668202eb9124b
Amber Kampen <akampen@g.hmc.edu>
2020-11-16 13:36:05 -0800

    updated database call with new database structure

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -51,7 +51,7 @@ class Map extends React.Component {
             const sensorVal = sensorMap[sensorID];
             // The label for this sensor is the most recent hour average
             // We strip to round to the ones place
-            const label = sensorVal.readings[0].toString().split('.')[0];
+            const label = sensorVal.nowCastPm25.toString().split('.')[0];
             const icon = createIcon(label);
 
             // Create marker
