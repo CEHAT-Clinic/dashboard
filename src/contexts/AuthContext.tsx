35b0c5ab4902884b0b78946aebfabe3bbe5dd6ed
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-21 14:44:40 -0800

    Add auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,48 @@
+import React, {createContext, useState, useContext} from 'react';
+import {Props} from './AppProviders';
+
+/**
+ * Interface for the auth context that defines the values contained by Auth
+ */
+interface AuthInterface {
+  isNewUser: boolean;
+  setIsNewUser(isNewUser: boolean): void;
+  authenticated: boolean;
+  setAuthenticated(authenticated: boolean): void;
+}
+
+/**
+ * Auth context with default values
+ */
+const AuthContext = createContext<AuthInterface>({} as AuthInterface);
+
+/**
+ * Authentication Provider that wraps App.tsx to provide Auth
+ * @param props child React components that will consume the context
+ */
+const AuthProvider: React.FC<Props> = ({children}: Props) => {
+  const defaultIsAuthorized = false;
+  const defaultIsNewUser = false;
+  const [authorized, setAuthorized] = useState(defaultIsAuthorized);
+  const [isNewUser, setIsNewUser] = useState(defaultIsNewUser);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isNewUser: isNewUser,
+        setIsNewUser: setIsNewUser,
+        authenticated: authorized,
+        setAuthenticated: setAuthorized,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+/**
+ * Custom hook to allow other components to use values described in AuthInterface
+ */
+const useAuth: () => AuthInterface = () => useContext(AuthContext);
+
+export {useAuth, AuthContext, AuthProvider};
