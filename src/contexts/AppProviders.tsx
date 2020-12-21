36e5e56b7182d23d14154e5bfc09dd3c9a94df5b
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-21 14:44:40 -0800

    Add auth context

--- src/contexts/AppProviders.tsx
+++ src/contexts/AppProviders.tsx
@@ -1,5 +1,6 @@
 import {ChakraProvider} from '@chakra-ui/react';
 import React from 'react';
+import {AuthProvider} from './AuthContext';
 
 /**
  * Interface to type the children components of the Providers.
@@ -14,7 +15,11 @@ interface Props {
  * @param props - React components wrapped by the providers
  */
 const AppProviders: React.FC<Props> = ({children}: Props) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </ChakraProvider>
+  );
 };
 
 export type {Props};
