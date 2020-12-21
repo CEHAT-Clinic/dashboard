e47619aa6d51ac4eb310944e385b1effbfd5590f
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-21 14:52:12 -0800

    Update Firebase file to fix secret keys and rename variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,29 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB,
   projectId: 'sg-cehat-air-quality',
-  storageBucket: process.env.REACT_APP_SB,
-  messagingSenderId: process.env.REACT_APP_SID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MID,
+  storageBucket: process.env.REACT_APP_FIREBASE_SB,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MID,
 };
 
-firebase.initializeApp(config);
-const db = firebase.firestore();
-export {db};
+// Avoid re-initializing Firebase if already initialized
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} else {
+  // Use app if Fireabse already initialized
+  firebase.app();
+}
+
+const firestore = firebase.firestore();
+const firebaseAuth = firebase.auth();
+
+export {firestore};
+export {firebaseAuth};
 export default firebase;
