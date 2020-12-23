dfec8f1f806338e4c99dc1d316250be656f85e3f
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-23 00:22:49 -0800

    Update Routes location and remove dead CSS

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,11 @@ import React, {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 // Import Pages: lazy imports to save on load time
-const About = lazy(() => import('../pages/About'));
-const Home = lazy(() => import('../pages/Home'));
-const Health = lazy(() => import('../pages/Health'));
-const Admin = lazy(() => import('../pages/Admin'));
-const Involved = lazy(() => import('../pages/Involved'));
+const About = lazy(() => import('./pages/About'));
+const Home = lazy(() => import('./pages/Home'));
+const Health = lazy(() => import('./pages/Health'));
+const Admin = lazy(() => import('./pages/Admin'));
+const Involved = lazy(() => import('./pages/Involved'));
 
 function Routes(): JSX.Element {
   return (
@@ -17,7 +17,7 @@ function Routes(): JSX.Element {
           <Route path="/health" component={Health} />
           <Route path="/involved" component={Involved} />
           <Route path="/admin" component={Admin} />
-          <Route path={['/home', '/']} component={Home} />
+          <Route path="/" component={Home} />
         </Switch>
       </Suspense>
     </Router>
