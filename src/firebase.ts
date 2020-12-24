219a91f901faab007db836b0a9ad9c943f5cdb9f
Carson Herness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-24 09:31:00 -0800

    Fix variable names to match GitHub secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import 'firebase/auth';
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: 'sg-cehat-air-quality',
-  storageBucket: process.env.REACT_APP_FIREBASE_SB,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Avoid re-initializing Firebase if already initialized
