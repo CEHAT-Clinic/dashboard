b58953d18197bf3e2dc4ff000501f4bd1aa56b53
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2020-12-24 10:34:47 -0800

    Update documentation

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import React, {createContext, useState, useContext} from 'react';
 import {Props} from './AppProviders';
 
 /**
- * Interface for the auth context that defines the values contained by Auth
+ * Interface for the auth context used for type safety
  */
 interface AuthInterface {
   isAuthenticated: boolean;
@@ -15,7 +15,8 @@ interface AuthInterface {
 const AuthContext = createContext<AuthInterface>({} as AuthInterface);
 
 /**
- * Authentication Provider that wraps App.tsx to provide Auth
+ * Authentication Provider that wraps App.tsx to provide authentication status
+ * throughout the entire app.
  * @param props child React components that will consume the context
  */
 const AuthProvider: React.FC<Props> = ({children}: Props) => {
@@ -37,7 +38,8 @@ const AuthProvider: React.FC<Props> = ({children}: Props) => {
 };
 
 /**
- * Custom hook to allow other components to use values described in AuthInterface
+ * Custom hook to allow other components to use and set authentication status
+ * Values: isAuthenticated (boolean) and setIsAuthenticated ((boolean) => void)
  */
 const useAuth: () => AuthInterface = () => useContext(AuthContext);
 
