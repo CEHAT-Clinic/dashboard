f634048d848429b9c8384e02e215c6f6f1ffb15a
Amber Kampen <akampen@g.hmc.edu>
2021-01-27 12:14:33 -0800

    pushing to share w/ Carson, cannot figure out responsive styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import Map from '../components/Map/Map';
 import './Home.css';
+import {
+  Text,
+  Heading,
+  Box,
+  Flex,
+  Spacer,
+  Divider,
+  extendTheme,
+} from '@chakra-ui/react';
+import {createBreakpoints} from '@chakra-ui/theme-tools';
 
 /**
  * Interface for the state of the home screen component
@@ -37,27 +47,38 @@ class Home extends React.Component<unknown, HomeState> {
 
   render(): JSX.Element {
     return (
-      <div>
-        <h1> Home Page</h1>
-        <h2>
+      <>
+        <Heading fontSize={['20px', '90px', '180px', '200px']}>
+          Home Page
+        </Heading>
+        <Text>
           NOTE: This Website Is Under Construction. Check Back in Spring 2021
-        </h2>
-        <div className="Content_Wrapper">
-          <div className="Map_Wrapper">
-            <Map updateSensor={this.updateState} />
-          </div>
-          <div className="Side_Widgets">
-            <div className="Dial">
-              <h2>The Dial Will Go Here</h2>
-              <text>Current Sensor Reading: {this.state.currentSensor}</text>
+        </Text>
+        <Flex direction={['column', 'column', 'row', 'row']}>
+          <Flex flex="2">
+            <div className="Map_Wrapper">
+              <Map updateSensor={this.updateState} />
             </div>
-            <div className="Multi_Purpose_Box">
-              <h2>The Multi Purpose Box Will Go Here</h2>
-              <text> Hello World</text>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Flex>
+          <Flex
+            bg="lemon"
+            direction="column"
+            alignItems="center"
+            textAlign="center"
+          >
+            <Text> Testing </Text>
+            <Box bg="tomato" w={['100%', '50%', '25%']}>
+              <Heading>The Dial Will Go Here</Heading>
+              <Text>Current Sensor Reading: {this.state.currentSensor}</Text>
+            </Box>
+            <Spacer />
+            <Box bg="lime">
+              <Heading>The Multi Purpose</Heading>
+              <Text>Hello World</Text>
+            </Box>
+          </Flex>
+        </Flex>
+      </>
     );
   }
 }
