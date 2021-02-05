b8acfdad6fdbe47149128c0cec669faa6969617d
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2021-02-05 10:49:48 -0800

    Add table to display admin users

--- src/components/Admin/UserPage/ManageUsers.tsx
+++ src/components/Admin/UserPage/ManageUsers.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import {Box, Heading, Flex, Button} from '@chakra-ui/react';
+import React, {useState} from 'react';
+import {
+  Box,
+  Heading,
+  Flex,
+  Button,
+  Table,
+  Thead,
+  Tr,
+  Td,
+  Th,
+  Tbody,
+  TableCaption,
+} from '@chakra-ui/react';
 import {useAuth} from '../../../contexts/AuthContext';
 import AccessDenied from './AccessDenied';
 import Loading from '../../Util/Loading';
+import {firestore} from '../../../firebase';
 
 /**
  * Component for administrative page to manage site users.
@@ -10,6 +23,28 @@ import Loading from '../../Util/Loading';
  */
 const ManageUsers: () => JSX.Element = () => {
   const {isAuthenticated, isAdmin, isLoading} = useAuth();
+  const [adminUsers, setAdminUsers] = useState<string[]>([]);
+
+  // Get all admin users
+  firestore
+  .collection('admin')
+  .doc('users')
+  .get()
+  .then(doc => {
+    if (doc.exists) {
+      // Get the document data that contains all admin userIds
+      const userData = doc.data();
+      if (userData) {
+        const adminUserIds: string[] = userData.userId ?? [];
+        setAdminUsers(adminUserIds);
+      }
+    }
+  })
+  .catch(error => {
+    // Error thrown upon failure to fetch the admin/users doc from Firestore
+    throw new Error(`Unable to fetch admin/users doc: ${error}`);
+  });
+
 
   if (isLoading) {
     return <Loading />;
@@ -31,6 +66,29 @@ const ManageUsers: () => JSX.Element = () => {
           textAlign="center"
         >
           <Heading>Manage Users</Heading>
+          <Table>
+            <TableCaption placement="top">Current Admin Users</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Name</Th>
+                <Th>Email</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+              </Tr>
+              <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+              </Tr>
+            </Tbody>
+          </Table>
           <Button as="a" href="/admin" margin={1}>
             Return to admin page
           </Button>
