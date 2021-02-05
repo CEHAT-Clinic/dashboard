d8148d15cb834de5a310df576ae6540d9eabbb10
Amber Kampen <akampen@g.hmc.edu>
2021-02-05 10:16:27 -0800

    switched to functional component w/ props

--- src/components/AQIDial.tsx
+++ src/components/AQIDial.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState, FunctionComponent} from 'react';
 import GaugeChart from 'react-gauge-chart';
 
 /**
- * Interface for the props of the dial, currentReading is the aqi value that
- * the dial should display
+ * Interface for the props of the dial
+ * - currentReading is the aqi value thatthe dial should display
  */
 interface DialProps {
   currentReading: string;
@@ -12,14 +12,18 @@ interface DialProps {
 /**
  * AQI Dial Display Component
  */
-class AQIDial extends React.Component<DialProps> {
+const AQIDial: ({currentReading}: DialProps) => JSX.Element = ({
+  currentReading,
+}: DialProps) => {
+  const [displayDial, setDisplayDial] = useState(false);
+
   /**
    * The graphics package displays a percentage (in range 0 to 1). This function
    * converts our AQI readings to the appropriate number between 0 and 1 that
    * the dial should display
    * @param currentReading - the aqi value to be displayed
    */
-  convertToPercent = (currentReading: string): number => {
+  const convertToPercent = (currentReading: string): number => {
     const aqi: number = +currentReading; // Convert from string to number
     let dialReading = 0; // Initialize dial reading
     if (aqi <= 50) {
@@ -38,22 +42,20 @@ class AQIDial extends React.Component<DialProps> {
     return dialReading;
   };
 
-  render(): JSX.Element {
-    return (
-      <>
-        <GaugeChart
-          id="currentSensorAQI"
-          nrOfLevels={5}
-          colors={['#1B8DFF', '#4765f5', '#9247a1', '#cc2475', '#FF3628']}
-          arcPadding={0.02}
-          percent={this.convertToPercent(this.props.currentReading)}
-          textColor={'black'}
-          hideText={true}
-          animate={false}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <GaugeChart
+        id="currentSensorAQI"
+        nrOfLevels={5}
+        colors={['#1B8DFF', '#4765f5', '#9247a1', '#cc2475', '#FF3628']}
+        arcPadding={0.02}
+        percent={convertToPercent(currentReading)}
+        textColor={'black'}
+        hideText={true}
+        animate={false}
+      />
+    </>
+  );
+};
 
 export default AQIDial;
