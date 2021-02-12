6ba48dc5d974cbb096ddc2b8262ee27193e0b112
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2021-02-12 12:26:55 -0600

    Externalize remaining pages

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,10 +1,14 @@
-import { Box, Heading } from '@chakra-ui/react';
+import {Box, Heading} from '@chakra-ui/react';
 import React from 'react';
+import {useTranslation} from 'react-i18next'
 
-const Health: React.FC = () => (
-  <Box>
-    <Heading> Health Info Page</Heading>
-  </Box>
-);
+const Health: React.FC = () => {
+  const {t} = useTranslation('health');
+  return (
+    <Box>
+      <Heading>{t('heading')}</Heading>
+    </Box>
+  );
+};
 
 export default Health;
