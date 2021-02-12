a04e23cbb398776a884ba360f331046d92e85e3f
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2021-02-12 12:31:18 -0600

    Properly semicolon

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,6 +1,6 @@
 import {Box, Heading} from '@chakra-ui/react';
 import React from 'react';
-import {useTranslation} from 'react-i18next'
+import {useTranslation} from 'react-i18next';
 
 const Health: React.FC = () => {
   const {t} = useTranslation('health');
