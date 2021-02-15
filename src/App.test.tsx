72670d968e7a245bb304b72523263f8d571d8598
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2021-02-15 15:42:25 -0600

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-i18next', () => ({
+  // This mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (s: string) => s,
+      i18n: {
+        /* eslint-disable @typescript-eslint/no-empty-function */
+        changeLanguage: () => new Promise(() => {}),
+        /* eslint-enable @typescript-eslint/no-empty-function */
+      },
+    };
+  },
+}));
+
 // Temporary test to have at least one test
-// Checks if "Home" appears in the app
-test('South Gate shows up on page', () => {
-  const {getByText} = render(<App />);
-  const home = getByText('Home');
-  expect(home).toBeInTheDocument();
+// Checks if appp renders without a failure
+test('App renders', () => {
+  render(<App />);  
 });
