2e753846b77558614098bb26474b13e28c57ef4b
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2021-02-19 13:33:47 -0600

    Add language toggle

--- src/components/NavBar/NavigationBar.tsx
+++ src/components/NavBar/NavigationBar.tsx
@@ -1,18 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import './NavigationBar.css';
 import cehatLogo from './CEHATLogo.png';
-import menuIcon from './menuIcon.png';
 import {useTranslation} from 'react-i18next';
+import {FaBars, FaGlobeAmericas} from 'react-icons/fa';
 
 function NavigationBar(): JSX.Element {
   // State of nav bar (always visible in large screen)
   const [isNavVisible, setIsNavVisible] = useState(true);
 
+  // State for whether to use globe or text, must be kept
+  // separate because mobile should always be text, even
+  // when nav bar is hidden
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 700px)').matches
+  );
+
   function handleScreenChange(this: MediaQueryList): void {
     if (this.matches) {
       setIsNavVisible(false);
+      setIsMobile(true);
     } else {
       setIsNavVisible(true);
+      setIsMobile(false);
     }
   }
 
@@ -35,20 +44,33 @@ function NavigationBar(): JSX.Element {
     setIsNavVisible(!isNavVisible);
   }
 
-  const {t} = useTranslation('menu');
+  function toggleLanguage(): void {
+    i18n.changeLanguage(i18n.language === 'en' ? 'es' : 'en');
+  }
+
+  const {t, i18n} = useTranslation('menu');
   return (
     <div>
       <header className="Navigation_Header">
-        {}
         {/* Logo in Spanish and English are the same */}
         <img src={cehatLogo} className="Logo" alt="Logo" />
-        {/* CEHAT logo */}
         {isNavVisible && (
           <nav className="Nav">
             <a href="/">{t('home')}</a>
             <a href="/health">{t('healthInfo')}</a>
             <a href="/about">{t('about')}</a>
             <a href="/admin">{t('admin')}</a>
+            <button id="changeLanguage" onClick={toggleLanguage}>
+              {/* Display globe on desktop, but text in menu bard on mobile */}
+              {isMobile ? (
+                'Change Language/Cambia Idioma'
+              ) : (
+                <FaGlobeAmericas
+                  aria-label="Change Language/Cambia Idioma"
+                  title="Change Language/Cambia Idioma"
+                />
+              )}
+            </button>
           </nav>
         )}
         <button onClick={toggleNav} className="Burger">
