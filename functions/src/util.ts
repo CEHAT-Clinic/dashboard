84afcdbc65dc4f3825160e4e37791e3b7e1f750a
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2021-04-16 11:00:43 -0700

    Add separate channel A and B errors'

--- functions/src/util.ts
+++ functions/src/util.ts
@@ -10,18 +10,21 @@ const readingsSubcollection: (docId: string) => string = (docId: string) =>
 
 /**
  * Errors that can occur with a sensor. All error types except `ReadingNotReceived`
- * result in a sensor being invalid.
+ * and `ChannelsDiverged` directly result in a sensor being invalid.
  * - `ReadingNotRecieved` - In the most recent recent call to PurpleAir, no
  *   reading was received for this sensor.
  * - `NoHumidityReading` - No humidity reading was received for a sensor that
  *   had a reading from PurpleAir. For this error, `IncompleteSensorReading` is
  *   also true.
+ * - `IncompleteSensorReading` - Some value for a sensor reading was not received.
  * - `ChannelsDiverged` - Channel A and Channel B of a PurpleAir sensor have
- *   diverged enough that the sensor values are not used. We determine if the
- *   channels have diverged by calculating the mean percent difference between
- *   the readings for the channels from the confidence value from PurpleAir.
- * - `SensorDowngraded` - If PurpleAir has downgraded Channel A, Channel B, or
- *   both channels of a sensor.
+ *   diverged enough that the most recent sensor reading should not be used.
+ *   We determine if the channels have diverged by calculating the mean percent
+ *   difference between the readings for the channels from the confidence value
+ *   from PurpleAir. The EPA recommends using a maximum mean percent difference
+ *   of 0.7 for valid sensor readings.
+ * - `ChannelADowngraded` - If PurpleAir has downgraded Channel A of a sensor.
+ * - `ChannelBDowngraded` - If PurpleAir has downgraded Channel B of a sensor.
  * - `InfiniteAqi`- If the calculated AQI for a sensor is infinite.
  * - `NotEnoughNewReadings` - If not enough new readings were received for a
  *   sensor in the last 3 hours. Two of the three most recent hours must have at
@@ -34,14 +37,15 @@ const readingsSubcollection: (docId: string) => string = (docId: string) =>
  *   recommended by the EPA for PurpleAir sensors.
  */
 enum SensorErrors {
-ReadingNotReceived,
-NoHumidityReading,
-IncompleteSensorReading,
-ChannelsDiverged,
-SensorDowngraded,
-InfiniteAqi,
-NotEnoughNewReadings,
-NotEnoughValidReadings
+  ReadingNotReceived,
+  NoHumidityReading,
+  IncompleteSensorReading,
+  ChannelsDiverged,
+  ChannelADowngraded,
+  ChannelBDowngraded,
+  InfiniteAqi,
+  NotEnoughNewReadings,
+  NotEnoughValidReadings
 }
 
 export {readingsSubcollection};
