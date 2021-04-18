5d3339bd6d003f002d2b745f1e0066b9d284452a
Amber Kampen <akampen@g.hmc.edu>
2021-04-18 09:46:47 -0700

    made loading nav bar bigger to match normal nav

--- src/components/NavBar/LoadingNavBar.tsx
+++ src/components/NavBar/LoadingNavBar.tsx
@@ -5,7 +5,7 @@ import {Box} from '@chakra-ui/react';
  * @returns component for the navigation bar at the top of the screen
  */
 function LoadingNavBar(): JSX.Element {
-  return <Box w="100%" h="3em" mb={2} p={3} background="#32bfd1" zIndex="3" />;
+  return <Box w="100%" h="4em" mb={2} p={3} background="#32bfd1" zIndex="3" />;
 }
 
 export default LoadingNavBar;
