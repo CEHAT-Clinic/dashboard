14faad07bf4e1ac0bafa52574ee0a0ef39a5b2f2
CarsonHerness <31805145+CarsonHerness@users.noreply.github.com>
2021-04-19 22:45:37 -0700

    Convert google and password user to context

--- src/components/Admin/ManageAccount/ChangeName.tsx
+++ src/components/Admin/ManageAccount/ChangeName.tsx
@@ -23,26 +23,13 @@ import {PasswordFormInput, SubmitButton} from '../ComponentUtil';
 import {firestore, firebaseAuth} from '../../../firebase/firebase';
 import {useTranslation} from 'react-i18next';
 import {handleReauthenticationWithPassword} from './Util';
-
-/**
- * Props for ChangeNameModal component. Used for type safety.
- */
-interface ChangeNameModalProps {
-  passwordUser: boolean;
-}
+import {useAuth} from '../../../contexts/AuthContext';
 
 /**
  * Component for changing an authenticated user's name. Includes button that
  * opens modal to update (or set for the first time) the user's name.
- * @param props - passwordUser
- * - `passwordUser`: if user uses a password for authentication, used to
- *                   reauthenticate if necessary before updating the user's name
  */
-const ChangeNameModal: ({
-  passwordUser,
-}: ChangeNameModalProps) => JSX.Element = ({
-  passwordUser,
-}: ChangeNameModalProps) => {
+const ChangeNameModal: () => JSX.Element = () => {
   // --------------- State maintenance variables ------------------------
   const {isOpen, onOpen, onClose} = useDisclosure();
 
@@ -58,6 +45,8 @@ const ChangeNameModal: ({
   // --------------- End state maintenance variables ------------------------
 
   const {t} = useTranslation(['administration', 'common']);
+
+  const {passwordUser} = useAuth();
 
   /**
    * Resets modal state values before closing the modal.
